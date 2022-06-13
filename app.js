8add1c51acb790b909c0f5962c732d84768838a8
박원균 <dev.calvinpark@gmail.com>
2022-06-13 19:19:43 +0900

    usage of ejs

--- app.js
+++ app.js
@@ -3,29 +3,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const page = __dirname;
-
-
 const app = express();
 
-app.get("/", (req, res)=>{
-  //get day
-  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-  const today = new Date();
-  
-  if(today.getDay() === 6 || today.getDay() === 0){
-    res.sendFile(page + "/index.html");
-    // res.write("It is weekend!");
-  }else {
-    let day = weekday[today.getDay()];
-    // res.write("<h1>Today is " + day + "</h1>");
-    // res.write("<h3>Go back to work!</h3>");
-    // res.send();
-    res.sendFile(page + "/index.html");
-  }
+app.set('view engine', 'ejs');
 
+app.get("/", (req, res) => {
+    //get day
+    const weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    const today = new Date();
+    let day = weekday[today.getDay()];
+    res.render("main/main", {kindOfDay: day});
 
 });
 
-app.listen(3000, ()=>{
-  console.log("Server is running on port 3000.");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
 });
