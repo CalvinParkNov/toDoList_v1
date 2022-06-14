023fddc91ef33d7b1955af0efb924b9a4844af18
박원균 <dev.calvinpark@gmail.com>
2022-06-14 14:23:29 +0900

    edit app.js

--- app.js
+++ app.js
@@ -7,22 +7,35 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    //get day
-    const weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ]
-    const today = new Date();
-    let day = weekday[today.getDay()];
-    res.render("main/main", {kindOfDay: day});
+app.use(bodyParser.urlencoded({extended: true}));
 
+var items = ['buy foods', 'cook foods', 'eat foods'];
+
+app.get("/", (req, res) => {
+
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    let day = today.toLocaleDateString("en-US", options);
+
+    let data = {
+        todayDate: day,
+        kindOfDay: 'Today',
+        addItem: items
+
+    };
+
+    res.render("list", data);
 });
+app.post("/", (req, res) => {
+    const item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
